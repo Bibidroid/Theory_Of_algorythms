508753bfdd1249434a9bc6b022dd2edf26649ecf
User <User@User-PC>
2021-04-15 14:14:08 +0300

    Lab1

--- src/main/java/knu/fit/ist/ta/SimpleApp.java
+++ src/main/java/knu/fit/ist/ta/SimpleApp.java
@@ -5,21 +5,31 @@
  */
 package knu.fit.ist.ta;
 
+import static java.lang.Math.PI;
+import java.text.DecimalFormat;
+
 /**
  *
  * @author User
  */
 public class SimpleApp {
 
-    /*public static void main(String[] args) {
-    MyFirstJavaClass mFJC = new MyFirstJavaClass(5);
+    public static void main(String[] args) {
+        /*MyFirstJavaClass mFJC = new MyFirstJavaClass(5);
     
     System.out.println( mFJC.toString());
     
     MyFirstJavaClass.setMyStaticInt(10);
     
-    System.out.println(MyFirstJavaClass.toStringStatic());
-    
-    }*/
+    System.out.println(MyFirstJavaClass.toStringStatic());*/
+
+        /*DecimalFormat df = new DecimalFormat("###.##");
+        System.out.println(df.format(PI));*/
+        
+        float f1=2.035678f;
+        
+        float f2=2.056587f;
+        System.out.println(Math.abs(f1-f2)<=0.01f);
+    }
 
 }
