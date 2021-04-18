c16b6229631a69a98adaa42bc4503e4ce94d389c
User-PC\User <oboz4500@gmail.com>
2021-04-18 12:31:53 +0300

    Refactoring v2

--- src/main/java/knu/fit/ist/ta/lab2/Lab2Servlet.java
+++ src/main/java/knu/fit/ist/ta/lab2/Lab2Servlet.java
@@ -6,17 +6,32 @@
 package knu.fit.ist.ta.lab2;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 /**
  *
- * @author User
+ * @author 38068
  */
+@WebServlet(name = "Lab2Servlet", urlPatterns = {"/lab2"})
 public class Lab2Servlet extends HttpServlet {
+    
+    /*@Autowired
+    Lab2View lab2view;
+    
+    
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+    super.init(config);
+    SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this,
+    config.getServletContext());
+    }*/
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -27,6 +42,8 @@ public class Lab2Servlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+    
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -39,7 +56,7 @@ public class Lab2Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-
+        
     }
 
     /**
@@ -53,7 +70,14 @@ public class Lab2Servlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        request.getRequestDispatcher("lab2.jsp").forward(request, response);
+        
+        Lab2View lab2view = new Lab2View();
+        
+        String result = lab2view.showResult(request.getParameter("x"));
+        request.setAttribute("result", result);        
+        request.getRequestDispatcher("lab2.jsp").forward(request,response);
+        
+        
     }
 
     /**
