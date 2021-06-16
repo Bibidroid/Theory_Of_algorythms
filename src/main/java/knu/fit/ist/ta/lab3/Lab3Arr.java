49a42bf97fdc59ec9eaf7fb05642c30f9508f8bd
User-PC\User <oboz4500@gmail.com>
2021-06-16 18:58:38 +0300

    EXAM

--- src/main/java/knu/fit/ist/ta/lab3/Lab3Arr.java
+++ src/main/java/knu/fit/ist/ta/lab3/Lab3Arr.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package knu.fit.ist.ta.lab3;
 
-/**
- *
- * @author User
- */
+
 public class Lab3Arr {
 
     private double min;
--- src/main/java/knu/fit/ist/ta/lab6/Progression.java
+++ src/main/java/knu/fit/ist/ta/lab6/Progression.java
@@ -1,4 +1,3 @@
-
 package knu.fit.ist.ta.lab6;
 
 import java.util.ArrayList;
@@ -6,23 +5,20 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-
 public class Progression {
-    public static String GetSequence(int length, int base, int degree)
-    {
+
+    public static String GetSequence(int length, int base, int degree) {
         List array = new ArrayList(length);
         int counter = length;
         GetSequenceRecursion(array, base, counter);
         Collections.reverse(array);
         return array.toString();
     }
-    
-    private static void GetSequenceRecursion(List array, int base, int counter)
-    {
-        if(counter == 0)
+
+    private static void GetSequenceRecursion(List array, int base, int counter) {
+        if (counter == 0) {
             return;
-        else
-        {
+        } else {
             array.add(Math.pow(base, counter));
             GetSequenceRecursion(array, base, counter - 1);
         }
